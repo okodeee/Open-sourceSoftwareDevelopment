f606eaa5964252715a31881818b6e6158d61b57e
jeong <init.jeongyj8443@gmail.com>
2021-09-17 13:47:06 +0900

    Change main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
 
 	int dan;
-	cout << "구구단 몇 단?";
+	cout << "구구단 몇 단?" << \t;
 	cin >> dan;
 
 	for (int i = 1; i < 10; i++) {
-		cout << dan << " * " << i << " = " << dan * i << endl;
+		cout << dan << " x " << i << " = " << dan * i << endl;
 	}
 
 
